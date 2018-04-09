2a3e8fb585cde5a0affe9265e221cf1ccc7a5037
Surendra Gandham <surendrag@FDLMC1708.local>
2018-04-09 17:52:45 +0530

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  
+  get "users/new"
 
-  get "static_pages/help"
+  #match '/', to: 'static_pages#home'
+  root to: 'static_pages#home'
 
-  get "static_pages/about"
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+
+  match '/about',   to: 'static_pages#about'
+
+  match '/contact', to: 'static_pages#contact'
 
   #match "static_pages"  => 'static_pages/home'
 
